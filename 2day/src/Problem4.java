83b4001abb1c7b2c67073d88714b8a6354ae57b5
NAM <NAM@DESKTOP-V83AK40>
2017-06-22 17:50:15 +0900

    fafa

--- 2day/src/Problem4.java
+++ 2day/src/Problem4.java
@@ -9,6 +9,7 @@ public class Problem4 {
 		System.out.println("당신의 나이는 " + a.nextInt() + "살 입니다.");
 		System.out.println("당신의 체중은 " + a.nextDouble() + "kg입니다.");
 		System.out.println("당신의 신장은 " + a.nextDouble() + "cm입니다.");
+
 	}
 
 }
