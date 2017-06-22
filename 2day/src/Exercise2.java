4a6f140e21f222878cc74ad3caa5e09ac14432a1
NAM <NAM@DESKTOP-V83AK40>
2017-06-22 19:08:57 +0900

    qqqq

--- 2day/src/Exercise2.java
+++ 2day/src/Exercise2.java
@@ -14,6 +14,7 @@ public class Exercise2 {
 	public static void main(String[] args) {
 		System.out.println("(0,0)에서 (3.4)까지 마우스를 드래그한 사각형의 면적은 " + rectangle(0,0,3,4) + " 입니다.");
 		System.out.println("원의 반지름이 3일 때 원의 면적은 " + circle(3) + " 입니다.");
+		System.out.println("sdf");
 	}
 
 }
